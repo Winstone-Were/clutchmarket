8900ed29946563df897bb944c00183c2cb0fcb7a
Winstone-Were <stoniedev@gmail.com>
2024-03-25 21:15:44 +0300

    Expo Router Config

---
+++ app/homepage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <View>
+      <Text>homepage</Text>
+    </View>
+  )
+}
+
+export default homepage
+
+const styles = StyleSheet.create({})
